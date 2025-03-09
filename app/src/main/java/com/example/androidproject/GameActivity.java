a3f345ae4a0a48688c4aa66e5f4869dfb71c24f1
brizzeta <76489220+brizzeta@users.noreply.github.com>
2025-03-09 21:50:49 +0200

    commit 09.03.2025

--- app/src/main/java/com/example/androidproject/GameActivity.java
+++ app/src/main/java/com/example/androidproject/GameActivity.java
@@ -1,48 +1,436 @@
 package com.example.androidproject;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.graphics.PorterDuff;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class GameActivity extends AppCompatActivity {
+import org.w3c.dom.Text;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Random;
+
+public class GameActivity extends AppCompatActivity {
+    private final Random random =  new Random();
+    private final String bestScoreFilename = "score.best";
+    private TextView tvScore;
+    private TextView tvBestScore;
+    private Animation spawnAnimation;
+    private Animation collapseAnimation;
+    private Animation bestScoreAnimation;
+    private final int N =4;
+    private final int[][] tiles = new int[N][N];
+    private long score;
+    private long bestScore;
+    private final TextView[][] tvTiles = new TextView[N][N];
+    private SavedState savedState;
+
+    @SuppressLint({"ClickableViewAccessibility", "DiscouragedApi"})
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_game);
-
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.game_layout_main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-        findViewById(R.id.game_layout_field).setOnTouchListener(
-                new OnSwipeListener(GameActivity.this) {
-                    @Override
-                    public void onSwipeBottom() {
-                        Toast.makeText(GameActivity.this, "OnSwipeBottom", Toast.LENGTH_SHORT).show();
-                    }
 
-                    @Override
-                    public void onSwipeLeft() {
-                        Toast.makeText(GameActivity.this, "OnSwipeLeft", Toast.LENGTH_SHORT).show();
-                    }
+        spawnAnimation = AnimationUtils.loadAnimation(this, R.anim.game_tile_spawn);
+        collapseAnimation = AnimationUtils.loadAnimation(this, R.anim.game_tile_collapse);
+        bestScoreAnimation = AnimationUtils.loadAnimation(this, R.anim.game_bestscore);
 
-                    @Override
-                    public void onSwipeRight() {
-                        Toast.makeText(GameActivity.this, "OnSwipeRight", Toast.LENGTH_SHORT).show();
-                    }
 
-                    @Override
-                    public void onSwipeTop() {
-                        Toast.makeText(GameActivity.this, "OnSwipeTop", Toast.LENGTH_SHORT).show();
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                tvTiles[i][j]= findViewById(
+                        getResources().getIdentifier(
+                                "game_tile_"+ i+j,
+                                "id",
+                                getPackageName()
+                        )
+                );
+            }
+
+        }
+        tvScore =findViewById(R.id.game_tv_score);
+        tvBestScore =findViewById(R.id.game_tv_best);
+        findViewById(R.id.game_btn_undo).setOnClickListener(this::undoClick);
+        findViewById(R.id.game_btn_new).setOnClickListener(this::newClick);
+        LinearLayout gameField = findViewById(R.id.game_layout_field);
+        gameField.post(()->{
+            int vw = this.getWindow().getDecorView().getWidth();
+            int fieldMargin= 20;
+            LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(
+                    vw- 2* fieldMargin,
+                    vw- 2* fieldMargin
+            );
+            layoutParams.setMargins(fieldMargin, fieldMargin, fieldMargin, fieldMargin);
+            layoutParams.gravity = Gravity.CENTER;
+            gameField.setLayoutParams(layoutParams);
+        });
+        gameField.setOnTouchListener(new OnSwipeListener(GameActivity.this){
+            @Override
+            public void onSwipeBottom() {
+                Toast.makeText(GameActivity.this, "onSwipeBottom", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onSwipeLeft() {
+                tryMakeMove(MoveDirection.left);
+            }
+
+            @Override
+            public void onSwipeRight() {
+                tryMakeMove(MoveDirection.right);
+            }
+
+            @Override
+            public void onSwipeTop() {
+                Toast.makeText(GameActivity.this, "onSwipeTop", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        bestScore = 0L;
+        loadBestScore();
+        startNewGame();
+    }
+
+
+    private void tryMakeMove(MoveDirection moveDirection){
+        boolean canMove = false;
+        switch(moveDirection){
+            case bottom:
+                break;
+            case left:
+                canMove = canMoveLeft(); break;
+            case right:
+                canMove = canMoveRight(); break;
+            case top:
+                break;
+        }
+        if(canMove){
+            saveField();
+            switch(moveDirection){
+                case bottom:
+                    break;
+                case left:
+                    moveLeft(); break;
+                case right:
+                    moveRight(); break;
+                case top:
+                    break;
+            }
+            spawnTile();
+            updateField();
+        }
+        else{
+            Toast.makeText(GameActivity.this, "NO move", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+
+    private void saveField(){
+        savedState = new SavedState(score, bestScore, new int[N][N]);
+        for (int i = 0; i < N; i++) {
+            System.arraycopy(tiles[i], 0, savedState.tiles[i], 0, N);
+        }
+    }
+
+    private void newClick(View view){
+        new AlertDialog.Builder(this, android.R.style.Theme_Material_Dialog_Alert)
+                .setIcon(android.R.drawable.ic_dialog_info)
+                .setMessage("Начать новую игру")
+                .setPositiveButton("Начать", (dlg, btn)-> {
+                    startNewGame();
+                })
+                .setNegativeButton("Отмена",(dlg, btn)-> finish() )
+                .setCancelable(false)
+                .show();
+    }
+
+    private void undoClick(View view){
+        if(savedState != null){
+            score = savedState.score;
+            bestScore = savedState.bestScore;
+            for (int i = 0; i < N; i++) {
+                System.arraycopy(tiles[i], 0, savedState.tiles[i], 0, N);
+            }
+            savedState = null;
+            updateField();
+        }
+        else{
+            new AlertDialog.Builder(this, android.R.style.Theme_Material_Dialog_Alert).setIcon(android.R.drawable.ic_dialog_alert).setTitle(R.string.game_tv_title).setMessage("Множинні збереження доступні за підпискою").setNeutralButton("Закрити", (dlg, btn)->{}).setPositiveButton("Підписка", (dlg, btn) -> {
+                Toast.makeText(this, "Скоро буде реалізовано", Toast.LENGTH_SHORT).show();
+            }).setNegativeButton("Вихід", (dlg,btn) -> finish()).setCancelable(false).show();
+        }
+    }
+
+    private void saveBestScore(){
+        try(FileOutputStream fos = openFileOutput(bestScoreFilename, Context.MODE_PRIVATE)) {
+            DataOutputStream writer = new DataOutputStream(fos);
+            writer.writeLong(bestScore);
+            writer.flush();
+        }
+        catch(IOException ex){
+            Log.w("GameActivity::saveBestScore", ex.getMessage() + " ");
+        }
+    }
+
+    private void loadBestScore(){
+        try(FileInputStream fis = openFileInput(bestScoreFilename);
+            DataInputStream reader = new DataInputStream(fis)){
+            bestScore = reader.readLong();
+        }
+        catch(IOException ex){
+            Log.w("GameActivity::loadBestScore", ex.getMessage() + " ");
+        }
+    }
+
+    private void startNewGame(){
+        score = 0L;
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                //tiles[i][j] = (int)Math.pow(2, i+j);
+                tiles[i][j]= 0;
+            }
+        }
+        spawnTile();
+        spawnTile();
+        updateField();
+    }
+
+    private boolean canMoveLeft(){
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if(tiles[i][j] != 0 && tiles[i][j+1] == tiles[i][j] ||
+                   tiles[i][j] == 0 && tiles[i][j + 1] != 0){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    private boolean canMoveRight(){
+        for (int i = 0; i < N; i++) {
+            for (int j = 1; j < N; j++) {
+                if(tiles[i][j] != 0 && tiles[i][j - 1] == tiles[i][j] ||
+                        tiles[i][j] == 0 && tiles[i][j - 1] != 0){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    private void moveRight(){
+        boolean res;
+        res = shiftRight(false);
+        for (int i = 0; i < N; i++) {
+            for (int j = N-1; j >0; j--) {
+                if(tiles[i][j] ==tiles[i][j-1] && tiles[i][j] !=0) {
+                    tiles[i][j] *=2;
+                    tiles[i][j-1] =0;
+                    score +=tiles[i][j];
+                    res =true;
+                    tvTiles[i][j].setTag(collapseAnimation);
+                }
+            }
+        }
+        if(res){
+            shiftRight(true);
+        }
+    }
+    private void moveLeft(){
+        boolean res;
+        res = shiftLeft(false);
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j <N-1; j++) {
+                if(tiles[i][j] ==tiles[i][j+1] && tiles[i][j] !=0) {
+                    tiles[i][j] *=2;
+                    tiles[i][j+1] =0;
+                    score +=tiles[i][j];
+                    res =true;
+                    tvTiles[i][j].setTag(collapseAnimation);
+                }
+            }
+        }
+        if(res){
+            shiftLeft(true);
+        }
+    }
+    private boolean shiftRight(Boolean shiftTags){
+        boolean res = false;
+        for (int i = 0; i < N; i++) {
+            boolean wasReplace ;
+            do{
+                wasReplace= false;
+                for (int j = 0; j < N-1; j++) {
+                    if(tiles[i][j] != 0 && tiles[i][j+1] == 0){
+                        tiles[i][j+1] = tiles[i][j];
+                        tiles[i][j]=0;
+                        wasReplace = true;
+                        res = true;
+                        if(shiftTags){
+                            Object tag= tvTiles[i][j].getTag();
+                            tvTiles[i][j].setTag(tvTiles[i][j+1].getTag() );
+                            tvTiles[i][j+1].setTag(tag);
+                        }
                     }
-                });
+                }
+            }while(wasReplace);
+        }
+        return res;
+    }
+    private boolean shiftLeft(Boolean shiftTags){
+        boolean res = false;
+        for (int i = 0; i < N; i++) {
+            boolean wasReplace ;
+            do{
+                wasReplace= false;
+                for (int j = 1; j < N; j++) {
+                    if(tiles[i][j] != 0 && tiles[i][j - 1] == 0){
+                        tiles[i][j - 1] = tiles[i][j];
+                        tiles[i][j]=0;
+                        wasReplace = true;
+                        res = true;
+                        if(shiftTags){
+                            Object tag= tvTiles[i][j].getTag();
+                            tvTiles[i][j].setTag(tvTiles[i][j - 1].getTag() );
+                            tvTiles[i][j - 1].setTag(tag);
+                        }
+                    }
+                }
+            }while(wasReplace);
+        }
+        return res;
+    }
+
+    private boolean spawnTile(){
+        boolean res = false;
+        List<Integer> freeTiles =  new ArrayList<>(N* N);
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if(tiles[i][j]==0){
+                    freeTiles.add(N* i +j);
+                }
+            }
+        }
+        if(freeTiles.isEmpty()){
+            return false;
+        }
+        int k = freeTiles.get(random.nextInt(freeTiles.size()));
+        int i = k/N;
+        int j = k%N;
+        tiles[i][j]= random.nextInt(10)==0 ? 4 : 2;
+        tvTiles[i][j].setTag( spawnAnimation);
+        return res;
+    }
+
+    float getFontSize(int value) {
+        if (value < 100) return 48.0f;
+        if (value < 1000) return 42.0f;
+        if (value < 10000) return 36.0f;
+        return 28.0f;
+    }
+
+    @SuppressLint("DiscouragedApi")
+    private void updateField(){
+        tvScore.setText(getString(R.string.game_tv_score_tpl, scoreToString(score) ));
+        tvBestScore.setText(getString(R.string.game_tv_best_tpl, scoreToString(bestScore) ));
+        if (score > bestScore) {
+            bestScore = score;
+            tvBestScore.setTag(bestScoreAnimation);
+            saveBestScore();
+        }
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                tvTiles[i][j].setText(scoreToString(tiles[i][j] ) );
+                tvTiles[i][j].setTextColor(
+                        getResources().getColor(
+                                getResources().getIdentifier(
+                                        String.format(Locale.ROOT,"game_tile%d_fg", tiles[i][j]),
+                                        "color",
+                                        getPackageName()
+                                ),
+                                getTheme()
+                        )
+                );
+
+                tvTiles[i][j].getBackground().setColorFilter(
+                        getResources().getColor(
+                                getResources().getIdentifier(
+                                        String.format(Locale.ROOT,"game_tile%d_bg", tiles[i][j]),
+                                        "color",
+                                        getPackageName()
+                                ),
+                                getTheme()
+                        ),
+                        PorterDuff.Mode.SRC_ATOP
+                );
+                tvTiles[i][j].setTextSize(getFontSize(tiles[i][j]));
+
+                Object animTag = tvTiles[i][j].getTag();
+                if(animTag instanceof Animation){
+                    tvTiles[i][j].startAnimation((Animation) animTag);
+                    tvTiles[i][j].setTag(null);
+                }
+                Object bestScoreTag = tvBestScore.getTag();
+                if (bestScoreTag instanceof Animation) {
+                    tvBestScore.startAnimation((Animation) bestScoreTag);
+                    tvBestScore.setTag(null);
+                }
+
+            }
+        }
+
+    }
+
+    private String scoreToString(long value){
+        return String.valueOf(value);
+    }
+
+
+    private static class SavedState{
+        private final int[][] tiles;
+        private final long score;
+        private final long bestScore;
+        private SavedState(long score, long bestScore, int[][] tiles){
+            this.bestScore = bestScore;
+            this.score = score;
+            this.tiles = tiles;
+        }
+    }
+
+    private enum MoveDirection{
+        bottom,
+        left,
+        right,
+        top
     }
 }
--- app/src/main/java/com/example/androidproject/MainActivity.java
+++ app/src/main/java/com/example/androidproject/MainActivity.java
@@ -3,67 +3,27 @@ package com.example.androidproject;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
-    private TextView tv1;
-
-    private TextView tvCounter;
-    private Button btnMinus, btnPlus;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        //ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-        //    Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-        //    v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-        //    return insets;
-        //});
-        tv1 = findViewById(R.id.textView1);
-        findViewById(R.id.button1).setOnClickListener(this::onButton1Click);
-        findViewById(R.id.button2).setOnClickListener(this::onButton2Click);
-        tvCounter = findViewById(R.id.tvCounter);
-        btnMinus = findViewById(R.id.btnMinus);
-        btnPlus = findViewById(R.id.btnPlus);
-        btnMinus.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                updateCounter(-1);
-            }
-        });
-
-        btnPlus.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                updateCounter(1);
-            }
-        });
+        findViewById( R.id.main_btn_calc).setOnClickListener(this::onButtonCalc);
+        findViewById( R.id.main_btn_game).setOnClickListener(this::onButtonGame);
+        findViewById( R.id.main_btn_anim).setOnClickListener(this::onButtonAnim);
     }
 
-    private void updateCounter(int delta) {
-        String currentText = tvCounter.getText().toString();
-        try {
-            int value = Integer.parseInt(currentText);
-            value += delta;
-            tvCounter.setText(String.valueOf(value));
-        } catch (NumberFormatException e) {
-            tvCounter.setText("10");
-        }
-    }
-
-    private void onButton1Click(View view) {
+    private void onButtonCalc(View view){
         startActivity(new Intent(MainActivity.this, CalcActivity.class));
     }
-    private void onButton2Click(View view) {
+    private void onButtonGame(View view){
         startActivity(new Intent(MainActivity.this, GameActivity.class));
     }
+    private void onButtonAnim(View view){
+        startActivity(new Intent(MainActivity.this, AnimActivity.class));
+    }
+
 }
